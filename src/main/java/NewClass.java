c25de7af9070747f4da779a901d51326d5a0ba3c
Shah Rukh Tahir <Shah Rukh Tahir@DESKTOP-0DLD6S7>
2022-04-15 22:18:39 +0500

    first commit

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Shah Rukh Tahir
+ */
+public class NewClass {
+    public static void main(String[] args){
+        String[][][][] timetable = new String[8][2][3][4];
+        
+        timetable[0][0][0][0] = "week-1";
+    }
+}
